9c1bd608f758a654f6d0aeeb17310d6c86cbb0a3
AuthenticPeach <51739720+AuthenticPeach@users.noreply.github.com>
2020-09-25 23:11:28 -0700

    Added json and main file
    Hello World in Java + Json

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+This is Hello World in Java
+
+*/
+public class HelloWorld {
+
+    public static void main(final String[] args){
+
+        System.out.println("Hello, World!!!");
+
+    }
+}
+
